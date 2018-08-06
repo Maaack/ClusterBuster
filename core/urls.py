1daca4cc922822fb5914210cae1106fbc1e80c3a
Maaack <marek.belski@gmail.com>
2018-08-06 14:49:37 -0700

    INIT-1: Removes stutter in view names.

--- core/urls.py
+++ core/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('games', views.GameListView.as_view(), name='games'),
-    path('game/<int:pk>', views.GameDetailView.as_view(), name='game_detail'),
-    path('player/create', views.PlayerCreateView.as_view(), name='player_create'),
-    path('player/<int:pk>', views.PlayerDetailView.as_view(), name='player_detail'),
-    path('player/<int:pk>/update', views.PlayerUpdateView.as_view(), name='player_update'),
+    path('games', views.GameList.as_view(), name='games'),
+    path('game/<int:pk>', views.GameDetail.as_view(), name='game_detail'),
+    path('player/create', views.PlayerCreate.as_view(), name='player_create'),
+    path('player/<int:pk>', views.PlayerDetail.as_view(), name='player_detail'),
+    path('player/<int:pk>/update', views.PlayerUpdate.as_view(), name='player_update'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -12,43 +12,43 @@ def index(request):
     return HttpResponse(template.render({}, request))
 
 
-class GameListView(generic.ListView):
+class GameList(generic.ListView):
     context_object_name = 'latest_game_list'
 
     def get_queryset(self):
         return Game.objects.order_by('-created')[:5]
 
 
-class GameDetailView(generic.DetailView):
+class GameDetail(generic.DetailView):
     model = Game
 
 
-class PlayerCreateView(generic.CreateView):
+class PlayerCreate(generic.CreateView):
     model = Player
     fields = ['name']
 
     def form_valid(self, form):
         self.request.session.save()
         form.instance.session_id = self.request.session.session_key
-        return super(PlayerCreateView, self).form_valid(form)
+        return super(PlayerCreate, self).form_valid(form)
 
     def get_success_url(self):
         return reverse('player_detail', kwargs={'pk': self.object.pk})
 
 
-class PlayerUpdateView(generic.UpdateView):
+class PlayerUpdate(generic.UpdateView):
     model = Player
     fields = ['name']
 
     def form_valid(self, form):
         self.request.session.save()
         form.instance.session_id = self.request.session.session_key
-        return super(PlayerUpdateView, self).form_valid(form)
+        return super(PlayerUpdate, self).form_valid(form)
 
     def get_success_url(self):
         return reverse('player_detail', kwargs={'pk': self.object.pk})
 
 
-class PlayerDetailView(generic.DetailView):
+class PlayerDetail(generic.DetailView):
     model = Player
 
