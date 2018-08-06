a99e06511baacaebdc55d61f7c21a52d396d6383
Maaack <marek.belski@gmail.com>
2018-08-06 14:48:38 -0700

    INIT-1: Adds generic Player views.

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('games', views.GameListView.as_view(), name='games'),
     path('game/<int:pk>', views.GameDetailView.as_view(), name='game_detail'),
+    path('player/create', views.PlayerCreateView.as_view(), name='player_create'),
+    path('player/<int:pk>', views.PlayerDetailView.as_view(), name='player_detail'),
+    path('player/<int:pk>/update', views.PlayerUpdateView.as_view(), name='player_update'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.views import generic
-from .models import Game
+from django.urls import reverse
+from .models import Game, Player
 
 
 # Create your views here.
@@ -20,4 +21,34 @@ class GameListView(generic.ListView):
 
 class GameDetailView(generic.DetailView):
     model = Game
+
+
+class PlayerCreateView(generic.CreateView):
+    model = Player
+    fields = ['name']
+
+    def form_valid(self, form):
+        self.request.session.save()
+        form.instance.session_id = self.request.session.session_key
+        return super(PlayerCreateView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('player_detail', kwargs={'pk': self.object.pk})
+
+
+class PlayerUpdateView(generic.UpdateView):
+    model = Player
+    fields = ['name']
+
+    def form_valid(self, form):
+        self.request.session.save()
+        form.instance.session_id = self.request.session.session_key
+        return super(PlayerUpdateView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('player_detail', kwargs={'pk': self.object.pk})
+
+
+class PlayerDetailView(generic.DetailView):
+    model = Player
 
