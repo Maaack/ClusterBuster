9e6b00e99d293269027fd09fda6eb4ee6f2949c1
Maaack <marek.belski@gmail.com>
2018-08-05 20:05:57 -0700

    INIT-1: Fix game detail view.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Game, Team, Player
+
+admin.site.register(Game)
+admin.site.register(Team)
+admin.site.register(Player)
