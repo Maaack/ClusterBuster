8ffa8348b75e73b0366161dab0111babd044cecb
Maaack <marek.belski@gmail.com>
2018-08-07 15:53:29 -0700

    INIT-1: Adds Word to the admin page.

--- core/admin.py
+++ core/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Game, Team, Player
+from .models import Game, Team, Player, Word
 
 admin.site.register(Game)
 admin.site.register(Team)
 admin.site.register(Player)
+admin.site.register(Word)
