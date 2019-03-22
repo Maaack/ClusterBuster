6f4eabe6a1dc5b5d716952a34fc44208f7c8e0a8
Maaack <marek.belski@gmail.com>
2019-03-22 16:00:12 -0700

    RFCT-03: Starting on the new form to submit for leader hints.

--- core/views/forms.py
+++ core/views/forms.py
@@ -1,1 +1,11 @@
 from django import forms
+
+
+class LeaderHintForm(forms.Form):
+    hint = forms.CharField(max_length=100)
+    position = forms.IntegerField(label='Position', disabled=True)
+
+    def __init__(self, *args, **kwargs):
+        super(LeaderHintForm, self).__init__(*args, **kwargs)
+        self.fields['position'].initial = 9
+        self.fields['hint'].initial = "Hint Here"
