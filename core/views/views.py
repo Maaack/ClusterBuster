2ddeca1e7ef8d477de47140a9f8b8c5b07a3bc7c
Maaack <marek.belski@gmail.com>
2019-03-22 22:17:57 -0700

    RFCT-03: Importing rooms.mixins to get player and room context data.

--- core/views/views.py
+++ core/views/views.py
@@ -6,6 +6,9 @@ from rooms.models import Room
 from games.models import Game
 from core.definitions import ClusterBuster
 
+from rooms.views.mixins import CheckPlayerView
+from rooms.views.contexts import PlayerContext, TeamContext, Player2RoomContext, Player2TeamContext
+
 from .forms import LeaderHintsForm
 
 
@@ -36,18 +39,50 @@ class UpdateGame(generic.RedirectView, generic.detail.SingleObjectMixin):
         return super().get_redirect_url(*args, **kwargs)
 
 
-class LeaderHintsFormView(generic.FormView):
+class LeaderHintsFormView(generic.FormView, CheckPlayerView):
     template_name = 'core/leader_hint_form.html'
     form_class = LeaderHintsForm
 
     def __init__(self):
         self.room = None
+        self.game = None
+        self.current_player = None
         super().__init__()
 
     def dispatch(self, request, *args, **kwargs):
-        room = get_object_or_404(Room, code=kwargs['slug'])
-        self.room = room
+        self.room = get_object_or_404(Room, code=kwargs['slug'])
+        self.game = Game.objects.filter(room=self.room).first()
+        self.current_player = self.get_current_player()
         return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        data = super(LeaderHintsFormView, self).get_context_data(**kwargs)
+        room = self.room
+        current_player = self.get_current_player()
+        if current_player:
+            player_data = PlayerContext.load(current_player)
+            data.update(player_data)
+            player_room_data = Player2RoomContext.load(current_player, room)
+            data.update(player_room_data)
+        players = room.players.all()
+        players_data = list()
+        for player in players:
+            player_data = PlayerContext.load(player)
+            player_room_data = Player2RoomContext.load(player, room)
+            player_data.update(player_room_data)
+            player_data['is_player'] = player == current_player
+            players_data.append(player_data)
+        data['players'] = players_data
+        teams = room.teams.all()
+        teams_data = list()
+        for team in teams:
+            team_data = TeamContext.load(team)
+            if current_player:
+                player_team_data = Player2TeamContext.load(current_player, team)
+                team_data.update(player_team_data)
+            teams_data.append(team_data)
+        data['teams'] = teams_data
+        return data
 
     def get_success_url(self):
         room = self.room
