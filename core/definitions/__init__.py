b90b83f377471dd7a800271b1fe6b8bdd01a3f44
Maaack <marek.belski@gmail.com>
2019-04-03 16:12:00 +0900

    RFCT-03: Updating the RuleLibrary of ClusterBuster to get further along.

--- core/definitions/__init__.py
+++ core/definitions/__init__.py
@@ -5,7 +5,7 @@ from core.basics import PatternDeckBuilder
 
 
 class ClusterBuster(RuleLibrary):
-    WINNING_TOKENS_REQUIRED_TO_WIN = {'key': 'winning_tokens_required_to_win', 'value': 2}
+    WINNING_TOKENS_REQUIRED_TO_WIN = 2
     LOSING_TOKENS_REQUIRED_TO_LOSE = 2
     STARTING_WIN_TOKENS_PER_TEAM = 0
     STARTING_LOSE_TOKENS_PER_TEAM = 0
@@ -67,15 +67,25 @@ class ClusterBuster(RuleLibrary):
         :return:
         """
         game_ready_state = State.objects.get(slug='game_play')
-        draw_words_stage_state = State.objects.get(slug='draw_words_stage')
         game.set_parameter_value('game_ready_state', game_ready_state)
+        draw_words_stage_state = State.objects.get(slug='draw_words_stage')
         game.set_parameter_value('draw_words_stage_state', draw_words_stage_state)
+        rounds_stage_state = State.objects.get(slug='rounds_stage')
+        game.set_parameter_value('rounds_stage_state', rounds_stage_state)
+        draw_code_card_stage_state = State.objects.get(slug='draw_code_card_stage')
+        game.set_parameter_value('draw_code_card_stage_state', draw_code_card_stage_state)
+        # Draw Words Trigger
         trigger = game.add_trigger('draw_words')
         condition_group = trigger.condition_group
         condition_group.set_to_and_op()
         condition_group.add_fsm_state_condition('fsm0', 'game_ready_state')
         condition_group.add_fsm_state_condition('fsm1', 'draw_words_stage_state')
-        game.transit_state_machine('fsm0', 'game_play', 'game_ready')
+        # Rounds Trigger
+        trigger = game.add_trigger('rounds_stage')
+        condition_group = trigger.condition_group
+        condition_group.add_fsm_state_condition('fsm1', 'rounds_stage_state')
+        # Game Ready Transition
+        game.transit_state_machine('fsm0', 'game_play', 'game ready')
 
     @staticmethod
     def team_won(game: Game):
@@ -83,7 +93,7 @@ class ClusterBuster(RuleLibrary):
         :param game:
         :return:
         """
-        game.transit_state_machine('fsm0', 'game_play', 'team_won')
+        game.transit_state_machine('fsm0', 'game_play', 'team won')
 
     @staticmethod
     def team_lost(game: Game):
@@ -91,63 +101,53 @@ class ClusterBuster(RuleLibrary):
         :param game:
         :return:
         """
-        game.transit_state_machine('fsm0', 'game_over', 'team_lost')
+        game.transit_state_machine('fsm0', 'game_over', 'team lost')
 
     @staticmethod
     def draw_words(game: Game):
-        teams_set = game.teams
-        team_count = teams_set.count()
-        total_words = ClusterBuster.SECRET_WORDS_PER_TEAM * team_count
-
-        random_word_set = Word.objects.order_by('?')
-        random_words = random_word_set.all()[:total_words]
-
         if not bool(game.get_parameter_value('word_cards_drawn')):
+            teams_set = game.teams
+            team_count = teams_set.count()
+            total_words = ClusterBuster.SECRET_WORDS_PER_TEAM * team_count
+            # Get Random Words
+            random_word_set = Word.objects.order_by('?')
+            random_words = random_word_set.all()[:total_words]
             for team_i, team in enumerate(teams_set.all()):
                 start_word_i = ClusterBuster.SECRET_WORDS_PER_TEAM * team_i
                 end_word_i = start_word_i + ClusterBuster.SECRET_WORDS_PER_TEAM
                 for word_i, random_word in enumerate(random_words[start_word_i:end_word_i]):
                     game.set_parameter_value(('team', team, 'secret_word', word_i+1), str(random_word))
             game.set_parameter_value('word_cards_drawn', True)
+        # Rounds Stage Transition
+        game.transit_state_machine('fsm1', 'rounds_stage', 'secret words drawn')
 
     @staticmethod
-    def secret_words_drawn(game: Game, state_machine: StateMachine):
-        conditional_transition = state_machine.add_conditional_transition('secret_words_drawn', 'rounds_stage')
-        conditional_transition.set_to_and_op()
-        for team in game.teams.all():
-            for i in range(ClusterBuster.SECRET_WORDS_PER_TEAM):
-                conditional_transition.add_has_value_condition(
-                    ('team', team, 'secret_word', i+1),
-                )
-
-    @staticmethod
-    def rounds_stage(game: Game, state_machine: StateMachine):
-        game.add_state_machine('first_round')
-        game.add_state_machine('select_leader_stage')
+    def rounds_stage(game: Game):
         game.set_parameter_value('current_round_count', 1)
         game.set_parameter_value('last_round_count', 8)
+        game.add_state_machine('fsm2', 'first_round')
+        game.add_state_machine('fsm3', 'select_leader_stage')
+        # Assign Team Leader Trigger
+        trigger = game.add_trigger('assign_team_leader')
+        condition_group = trigger.condition_group
+        condition_group.add_fsm_state_condition('fsm3', 'select_leader_stage')
 
     @staticmethod
-    def assign_team_leader(game: Game, state_machine: StateMachine):
+    def assign_team_leader(game: Game):
         round_number = game.get_parameter_value('current_round_count')
         for team in game.teams.all():
             player_count = team.players.count()
             offset = round_number % player_count
             round_leader = team.players.all()[offset]
             game.set_parameter_value(('round', round_number, 'team', team, 'leader'), round_leader)
+        game.transit_state_machine('fsm3', 'draw_code_card_stage', 'team leaders assigned')
+        # Assign Team Leader Trigger
+        trigger = game.add_trigger('leaders_draw_code_numbers')
+        condition_group = trigger.condition_group
+        condition_group.add_fsm_state_condition('fsm3', 'draw_code_card_stage_state')
 
     @staticmethod
-    def team_leaders_assigned(game: Game, state_machine: StateMachine):
-        round_number = game.get_parameter_value('current_round_count')
-        conditional_transition = state_machine.add_conditional_transition('team_leader_assigned', 'draw_code_card_stage')
-        conditional_transition.set_to_and_op()
-        for team in game.teams.all():
-            conditional_transition.add_has_value_condition(
-                ('round', round_number, 'team', team, 'leader'),
-            )
-
-    @staticmethod
-    def leaders_draw_code_numbers(game: Game, state_machine: StateMachine):
+    def leaders_draw_code_numbers(game: Game):
         round_number = game.get_parameter_value('current_round_count')
         for team in game.teams.all():
             deck = PatternDeckBuilder.build_deck()
@@ -205,10 +205,8 @@ class ClusterBuster(RuleLibrary):
             'win_condition': ClusterBuster.set_win_condition,
             'lose_condition': ClusterBuster.set_lose_condition,
             'draw_words': ClusterBuster.draw_words,
-            'secret_words_drawn': ClusterBuster.secret_words_drawn,
             'rounds_stage': ClusterBuster.rounds_stage,
             'assign_team_leader': ClusterBuster.assign_team_leader,
-            'team_leaders_assigned': ClusterBuster.team_leaders_assigned,
             'leaders_draw_code_numbers': ClusterBuster.leaders_draw_code_numbers,
             'code_numbers_drawn': ClusterBuster.code_numbers_drawn,
             'leaders_made_hints': ClusterBuster.leaders_made_hints,
