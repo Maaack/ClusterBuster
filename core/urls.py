ab36db291d1506e2f32fa10369ff258d12200040
Maaack <marek.belski@gmail.com>
2019-04-09 22:06:54 -0700

    RFCT-03: Fix URLs to have `/` at the end.

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('rooms/<slug:slug>/start_game', views.StartGame.as_view(), name='start_game'),
-    path('games/<slug:slug>', views.GameDetail.as_view(), name='game_detail'),
+    path('games/<slug:slug>/', views.GameDetail.as_view(), name='game_detail'),
     path('games/<slug:slug>/update_game', views.UpdateGame.as_view(), name='update_game'),
     path('games/<slug:slug>/leader_hints', views.LeaderHintsFormView.as_view(), name='leader_hints'),
     path('games/<slug:slug>/player_guesses', views.PlayerGuessesFormView.as_view(), name='player_guesses'),
--- rooms/urls.py
+++ rooms/urls.py
@@ -8,11 +8,11 @@ from .views import (
 urlpatterns = [
     path('', index_view, name='index'),
     path('new_player/', PlayerCreate.as_view(), name='player_create'),
-    path('players/<int:pk>', PlayerDetail.as_view(), name='player_detail'),
+    path('players/<int:pk>/', PlayerDetail.as_view(), name='player_detail'),
     path('players/<int:pk>/update', PlayerUpdate.as_view(), name='player_update'),
     path('new_room/', RoomCreate.as_view(), name='room_create'),
     path('rooms/', RoomList.as_view(), name='room_list'),
-    path('rooms/<slug:slug>', RoomDetail.as_view(), name='room_detail'),
+    path('rooms/<slug:slug>/', RoomDetail.as_view(), name='room_detail'),
     path('rooms/<slug:slug>/new_player', CreatePlayerAndJoinRoom.as_view(), name='player_create_and_join_room'),
     path('rooms/<slug:slug>/join', JoinRoom.as_view(), name='join_room'),
 ]
