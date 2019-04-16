1bf72465b84fd3dcb4c71fba92dd1586b4901ade
Maaack <marek.belski@gmail.com>
2019-04-15 18:43:37 -0700

    GAME-07: Fix all these URLs.

--- core/urls.py
+++ core/urls.py
@@ -3,13 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('rooms/<slug:slug>/start_game', views.StartGame.as_view(), name='start_game'),
+    path('rooms/<slug:slug>/start_cluster_buster/', views.StartGame.as_view(), name='start_cluster_buster'),
     path('games/<slug:slug>/', views.GameDetail.as_view(), name='game_detail'),
-    path('games/<slug:slug>/update_game', views.UpdateGame.as_view(), name='update_game'),
-    path('games/<slug:slug>/leader_hints', views.LeaderHintsFormView.as_view(), name='leader_hints'),
-    path('games/<slug:slug>/player_guesses', views.PlayerGuessesFormView.as_view(), name='player_guesses'),
-    path('games/<slug:slug>/player_guesses_opponents', views.PlayerGuessesOpponentHintsFormView.as_view(), name='player_guesses_opponents'),
-    path('games/<slug:slug>/start_next_round', views.StartNextRound.as_view(), name='start_next_round'),
-    path('games/<slug:slug>/score_teams', views.ScoreTeams.as_view(), name='score_teams'),
+    path('games/<slug:slug>/update_game/', views.UpdateGame.as_view(), name='update_game'),
+    path('games/<slug:slug>/leader_hints/', views.LeaderHintsFormView.as_view(), name='leader_hints'),
+    path('games/<slug:slug>/player_guesses/', views.PlayerGuessesFormView.as_view(), name='player_guesses'),
+    path('games/<slug:slug>/player_guesses_opponents/', views.PlayerGuessesOpponentHintsFormView.as_view(), name='player_guesses_opponents'),
+    path('games/<slug:slug>/start_next_round/', views.StartNextRound.as_view(), name='start_next_round'),
+    path('games/<slug:slug>/score_teams/', views.ScoreTeams.as_view(), name='score_teams'),
 
 ]
