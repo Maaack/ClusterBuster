e8f8a2d25304f244fcdcb1d348faeaabd1221a37
Maaack <marek.belski@gmail.com>
2019-04-22 20:51:34 -0700

    GAME-11: Clean up imports.

--- games/models/mixins/__init__.py
+++ games/models/mixins/__init__.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
-from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 class BaseValue(models.Model):
