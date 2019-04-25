05673ef0fb1f8785056b16e4c1c0b466fcbb05dd
Maaack <marek.belski@gmail.com>
2019-04-24 21:18:56 -0700

    GAME-14: NO MORE RULES!

--- gamedefinitions/models/definitions.py
+++ gamedefinitions/models/definitions.py
@@ -4,22 +4,6 @@ from django.utils.translation import ugettext_lazy as _
 from clusterbuster.mixins import TimeStamped
 
 
-class Rule(TimeStamped):
-    """
-    Rules define how the game is played.
-    """
-    slug = models.SlugField(_("Slug"), max_length=64)
-    description = models.TextField(_("Description"), default='')
-
-    class Meta:
-        verbose_name = _("Rule")
-        verbose_name_plural = _("Rules")
-        ordering = ["-created"]
-
-    def __str__(self):
-        return str(self.slug)
-
-
 class GameDefinition(TimeStamped):
     slug = models.SlugField(_("Slug"), max_length=64)
     title = models.CharField(_("Title"), max_length=128, default='')
