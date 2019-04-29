1477866d66eb0afc5a752a86f6cf1bdeb1ba8fe9
Maaack <marek.belski@gmail.com>
2019-04-28 18:09:07 -0700

    GAME-17: New models for Code Cards and Decks.

--- core/admin.py
+++ core/admin.py
@@ -7,4 +7,6 @@ from . import models
 admin.site.register(models.Word)
 admin.site.register(models.State)
 admin.site.register(models.StateMachine)
+admin.site.register(models.CodeCard)
+admin.site.register(models.Deck)
 admin.site.register(models.ClusterBuster)
