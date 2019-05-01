140f27d5ee19a765cee7c3b92c1d593aa192939b
Maaack <marek.belski@gmail.com>
2019-05-01 08:44:50 -0700

    API-01: Update settings.

--- clusterbuster/settings.py
+++ clusterbuster/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
+    'crispy_forms',
+    'guardian',
+    'rest_framework',
     'lobbies',
     'games',
     'core',
@@ -84,6 +88,10 @@ DATABASES = {
     }
 }
 
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend', # default
+    'guardian.backends.ObjectPermissionBackend',
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
